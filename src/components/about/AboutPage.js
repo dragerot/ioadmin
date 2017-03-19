eb803085a1baead0e127733b05e978cb3c069836
toregard <dragerot@gmail.com>
2017-03-20 00:23:50 +0100

    lagt inn

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is a IoT project that show e-helse demo</p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
